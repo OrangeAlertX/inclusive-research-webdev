ffff9b5269134f828d71479a387d87e216fb52dc
bogdan_sevruk <orangealertx@gmail.com>
2023-09-29 23:57:12 +0300

    init Evclid

---
+++ Evclid/js/swiper.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const swiper = new Swiper('.swiper', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+});
