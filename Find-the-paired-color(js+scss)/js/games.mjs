0c41e5d8eb741e8121dad9f4d9993457d6bf2716
bogdan_sevruk <orangealertx@gmail.com>
2023-10-06 19:28:03 +0300

    MVP

--- Find-the-paired-color(js+scss)/js/games.mjs
+++ Find-the-paired-color(js+scss)/js/games.mjs
@@ -1,13 +1,22 @@
 import { newElem } from './game-tools.mjs';
 
-const createEasyGame = (playground) => {
-  const qtEasyCards = 16;
-  const bunchSize = 2;
+const createGame = (e, playground, qtCards, bunchSize) => {
   const grid = newElem('div', 'grid');
-  const cards = createCards(qtEasyCards, bunchSize);
+  const cards = createCards(qtCards, bunchSize);
   grid.append(...cards);
+  playground.innerHTML = '';
   playground.append(grid);
+  changeOrder(grid);
+  setGridSizes(grid, qtCards);
   initFlipEvents(grid, bunchSize);
+
+  const root = document.querySelector(':root');
+  root.style.setProperty(
+    '--currentDifficulty',
+    window
+      .getComputedStyle(e.currentTarget)
+      .getPropertyValue('background-color')
+  );
 };
 
 const createCards = (qt, bunchSize) => {
@@ -22,6 +31,7 @@ const createCards = (qt, bunchSize) => {
     card.append(cardBody);
     cardBody.append(cardFace, cardBack);
     card.setAttribute('bunchID', `bunch${i + 1}`);
+    cardBack.textContent = `${i}`;
     cardBack.style.backgroundColor = `rgb(${getColor()},${getColor()},${getColor()})`;
     cards.push(card);
     for (let i = 1; i < bunchSize; i++) {
@@ -37,6 +47,7 @@ const getColor = () => Math.floor(Math.random() * 256);
 const initFlipEvents = (grid, bunchSize) => {
   const cards = grid.children;
   const memory = [];
+  grid.setAttribute('counter', grid.children.length);
   for (let i = 0; i < cards.length; i++) {
     let card = cards[i];
     card.addEventListener('click', () =>
@@ -46,12 +57,11 @@ const initFlipEvents = (grid, bunchSize) => {
 };
 
 const actionFlipCard = (card, bunchSize, memory) => {
-  console.log(memory);
   if (!flipTheCard(card, memory)) return;
   memory.push(card);
 
   cardsMatching(memory, card);
-  if (memory.length === bunchSize) acceptCards(memory);
+  if (memory.length === bunchSize) acceptCards(memory, card);
 };
 
 const flipTheCard = (card, memory) => {
@@ -67,11 +77,12 @@ const flipTheCard = (card, memory) => {
 const cardsMatching = (memory, card) => {
   for (const cardM of memory) {
     if (cardM.getAttribute('bunchID') !== card.getAttribute('bunchID')) {
-      clearGridMemory(memory, card.parentElement); //setTimeout
+      clearGridMemory(memory, card.parentElement);
     }
   }
 };
 const clearGridMemory = (memory, grid) => {
+  const remain = memory[memory.length - 1];
   for (const cardM of memory) {
     flipTheCard(cardM, memory);
   }
@@ -79,12 +90,41 @@ const clearGridMemory = (memory, grid) => {
   for (const card of grid.children) {
     if (card.children[0].style.transform) card.children[0].style.transform = '';
   }
+  remain.click();
 };
-const acceptCards = (memory) => {
+const acceptCards = (memory, card) => {
   for (const cardM of memory) {
-    cardM.style.visibility = 'hidden';
+    setTimeout(() => {
+      cardM.style.visibility = 'hidden';
+    }, 1000);
   }
+
+  const cur = parseInt(card.parentElement.getAttribute('counter'));
+  if (cur <= memory.length)
+    setTimeout(() => {
+      actionWinGame();
+    }, 1500);
+  else card.parentElement.setAttribute('counter', cur - memory.length);
+
   memory.length = 0;
 };
 
-export { createEasyGame };
+const changeOrder = (grid) => {
+  for (const card of grid.children) {
+    card.style.order = `${parseInt(Math.random() * 100)}`;
+  }
+};
+
+const actionWinGame = () => {
+  alert('You win!');
+};
+
+const setGridSizes = (grid, qt) => {
+  const sizes = { 8: 4, 12: 3, 18: 6, 24: 6, 32: 8, 30: 10 };
+  let columns = sizes[qt];
+  if (!columns) columns = 8;
+
+  grid.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+};
+
+export { createGame };
--- Find-the-paired-color(js+scss)/js/script.js
+++ Find-the-paired-color(js+scss)/js/script.js
@@ -1,10 +1,35 @@
 import { initGameItems } from './game-tools.mjs';
-import { createEasyGame } from './games.mjs';
+import { createGame } from './games.mjs';
 
-const createGame = () => {
+const createGameWrapper = () => {
   const container = initGameItems();
   document.body.append(container);
-  createEasyGame(container.children[1]);
+  setGameGreation(container);
 };
 
-createGame();
+const setGameGreation = (container) => {
+  const difficulties = container.children[0];
+  difficulties.children[0].addEventListener('click', (e) => {
+    createGame(e, container.children[1], 8, 2);
+  });
+  difficulties.children[1].addEventListener('click', (e) => {
+    createGame(e, container.children[1], 12, 2);
+  });
+  difficulties.children[2].addEventListener('click', (e) => {
+    createGame(e, container.children[1], 18, 3);
+  });
+  difficulties.children[3].addEventListener('click', (e) => {
+    createGame(e, container.children[1], 24, 3);
+  });
+  difficulties.children[4].addEventListener('click', (e) => {
+    createGame(e, container.children[1], 24, 4);
+  });
+  difficulties.children[5].addEventListener('click', (e) => {
+    createGame(e, container.children[1], 32, 4);
+  });
+  difficulties.children[6].addEventListener('click', (e) => {
+    createGame(e, container.children[1], 30, 6);
+  });
+};
+
+createGameWrapper();
