dfb448942ab5804984564cf9ad5d20d607ee0d9a
bogdan_sevruk <orangealertx@gmail.com>
2023-10-12 22:27:45 +0300

    finished section one with api Swiper

--- Evclid/js/script.js
+++ Evclid/js/script.js
@@ -23,6 +23,19 @@
 // Header //
 (() => {
   'use strict';
+  //Active button when input in focus //
+  const input = document.querySelector('.header__input');
+  const submit = document.querySelector('.header__btn');
+
+  input.addEventListener('focus', () => {
+    submit.classList.add('active');
+  });
+
+  input.addEventListener('blur', () => {
+    submit.classList.remove('active');
+  });
+
+  //open-close
   const nav = document.querySelector('.header__nav');
   const bar = document.querySelector('.header__bar');
 
@@ -39,9 +52,45 @@
   });
 
   const search = document.querySelector('.header__search');
-  search.addEventListener('click', () => switcher(bar));
+  search.addEventListener('click', () => {
+    switcher(bar);
+    input.focus();
+  });
 
   const switcher = (el) => {
     el.classList.toggle(`${el.classList[0]}--active`);
   };
+})();
+
+// Section-1-Order
+(() => {
+  'use strict';
+  // download image based on innerWidth
+  const images = document.getElementsByClassName('order__img');
+
+  window.addEventListener('resize', initImages);
+
+  initImages();
+
+  function initImages() {
+    const width = window.innerWidth;
+
+    for (let i = 0; i < 3; i++) {
+      setSrc(images[i], i + 1, width);
+    }
+  }
+
+  function setSrc(image, i, width) {
+    let choice;
+    if (width <= 320) {
+      choice = '-320';
+    } else if (width <= 768) {
+      choice = '-768';
+    } else if (width <= 1024) {
+      choice = '-1024';
+    } else {
+      choice = '';
+    }
+    image.setAttribute('src', `images/order/order-${i}${choice}.jpg`);
+  }
 })();
--- Evclid/js/swiper.js
+++ Evclid/js/swiper.js
@@ -1,5 +1,10 @@
 const swiper = new Swiper('.swiper', {
   loop: true,
+  // autoplay: {
+  //   delay: 3500,
+  //   disableOnInteraction: false,
+  // },
+  speed: 1000,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
