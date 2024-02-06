d31cae7bbdd4e8e923c4bfaf5d0d4baa650fd665
bogdan_sevruk <orangealertx@gmail.com>
2024-02-06 20:41:50 +0300

    feb6

--- ReactTemplates/index.js
+++ ReactTemplates/index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import ncp from 'ncp';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -5,19 +7,13 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const target = process.argv.slice(2)[0];
-
-if (!target || target.slice(0, 2) !== './') {
-  console.log(`cannot resolve to the "${target}", use "./<path>"`);
-  process.exit(9);
-}
+const target = './';
 
 ncp(
   path.resolve(__dirname, './ReactTemplateVite'),
   path.resolve(process.cwd(), target),
   {
     stopOnErr: true,
-    clobber: false,
     filter: (filePath) => {
       const file = filePath.split('/').at(-1);
       if (file === 'node_modules' || file === 'dist') return false;
