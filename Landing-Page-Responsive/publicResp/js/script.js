a4b4a80804aa08812bbd8656d4190cde349a1ba6
bogdan_sevruk <orangealertx@gmail.com>
2024-02-17 22:56:00 +0300

    feb17

--- Landing-Page-Responsive/publicResp/js/script.js
+++ Landing-Page-Responsive/publicResp/js/script.js
@@ -40,24 +40,24 @@
   const bar = document.querySelector('.header__bar');
 
   const button = document.querySelector('.header__burger');
-  button.addEventListener('click', () => switcher(nav));
+  button.addEventListener('click', () => toggle(nav));
 
   const close = document.querySelector('.header__close');
-  close.addEventListener('click', () => switcher(nav));
+  close.addEventListener('click', () => toggle(nav));
 
   const returner = document.querySelector('.header__return');
   returner.addEventListener('click', (e) => {
     e.preventDefault();
-    switcher(bar);
+    toggle(bar);
   });
 
   const search = document.querySelector('.header__search');
   search.addEventListener('click', () => {
-    switcher(bar);
+    toggle(bar);
     input.focus();
   });
 
-  const switcher = (el) => {
+  const toggle = (el) => {
     el.classList.toggle(`${el.classList[0]}--active`);
   };
 })();
@@ -67,7 +67,37 @@
   'use strict';
   // download image based on innerWidth
   const images = document.getElementsByClassName('order__img');
+  addImgResizer(images, 'order');
+})();
 
+// Section-3-details
+(() => {
+  'use strict';
+
+  const buttons = [...document.querySelectorAll('.details__item')];
+  const left = [...document.querySelectorAll('.details__leftchange')];
+  const right = [...document.querySelectorAll('.details__rightchange')];
+
+  buttons.forEach((button, i) =>
+    button.addEventListener('click', () => {
+      buttons.forEach((b) => b.classList.remove('details__item--active'));
+      button.classList.add('details__item--active');
+      disableAll(left);
+      disableAll(right);
+      left[i].classList.remove('disable');
+      right[i].classList.remove('disable');
+    })
+  );
+
+  function disableAll(elements) {
+    elements.forEach((element) => element.classList.add('disable'));
+  }
+  //download images
+  const images = document.querySelectorAll('.details__rightchange > img');
+  addImgResizer(images, 'details');
+})();
+
+function addImgResizer(images, folderName) {
   window.addEventListener('resize', initImages);
 
   initImages();
@@ -75,7 +105,7 @@
   function initImages() {
     const width = window.innerWidth;
 
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < images.length; i++) {
       setSrc(images[i], i + 1, width);
     }
   }
@@ -93,7 +123,7 @@
     }
     image.setAttribute(
       'src',
-      `publicResp/images/order/order-${i}${choice}.jpg`
+      `publicResp/images/${folderName}/${folderName}-${i}${choice}.jpg`
     );
   }
-})();
+}
