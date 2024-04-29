a63faaec76329edb2f31cc9002f6c49e2c5920e1
bogdan_sevruk <orangealertx@gmail.com>
2024-04-29 13:06:29 +0300

    apr29

--- Core/src/components/App/App.tsx
+++ Core/src/components/App/App.tsx
@@ -1,15 +1,26 @@
-import { Route, Routes } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Viewer from '../../../../ComponentViewer/src/components/Viewer/Viewer';
 import Core from '../Core/Core';
 import AppCollection from '../../../../ComponentCollection/src/components/App/App';
 
 export { Viewer };
 
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Core />,
+      children: [
+        {
+          path: 'collection',
+          element: <AppCollection />,
+        },
+      ],
+    },
+  ],
+  { basename: '/inclusive-research-webdev' }
+);
+
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Core />} />
-      <Route path="/collection" element={<AppCollection />} />
-    </Routes>
-  );
+  return <RouterProvider router={router} />;
 }
--- Core/vite.config.ts
+++ Core/vite.config.ts
@@ -2,8 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { Mode, plugin as mdReader } from 'vite-plugin-markdown';
 
+const BASE = process.env.BASE;
+
+console.log('BASE: ', BASE);
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: BASE ?? '/',
   build: {
     rollupOptions: {
       external: [/^node:.*/],
